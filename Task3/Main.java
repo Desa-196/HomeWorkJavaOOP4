52bbdda7ec9e8b000c476df0a0cc888c5fc794e3
Денис Пономарев <deskin196@gmail.com>
2023-07-23 12:23:11 +0300

    Реализовал метод getMax

--- Task3/Main.java
+++ Task3/Main.java
@@ -11,6 +11,17 @@
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getMax(5, 5));
+        System.out.println(getMax(5, 15));
+        System.out.println(getMax(10, 5));
+    }
 
+    public static <T extends Comparable<T>> T getMax(T element1, T element2) {
+        // Если больше нуля значит element1 > element2 - возвращаем element1, если 0 то
+        // просто вернём один из элементов - element1
+        if (element1.compareTo(element2) >= 0)
+            return element1;
+        else
+            return element2;
     }
 }
